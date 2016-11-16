4dd71b7b3edb0cd742b4aea7900398e0d51ccd1a
Nick Phillips <nicktgphillips@gmail.com>
2016-11-16 15:47:39 +0000

    added basic test framework with mocha, chai, sinon, and enzyme

---
+++ test/components/app.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';                     // required to get test to work.  we can get around this later with more configuration
+import { shallow } from 'enzyme';              // method from enzyme which allows us to do shallow render
+import App from '../../src/components/app';  // import our soon to be component
+
+describe('(Component) App', () => {
+  it('renders as a <h1>', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.type()).to.eql('h1');
+  });
+});
